0e16f711df6feda71a1fa21a2adce1adfec8de54
Alexey Nagornov <al.nagornov@gmail.com>
2019-07-24 18:34:29 +0400

    comment

--- src/modules/core/store/users-store.service.ts
+++ src/modules/core/store/users-store.service.ts
@@ -3,7 +3,8 @@ import { BehaviorSubject } from 'rxjs';
 import { User } from 'src/interfaces';
 
 /**
- * Bette would be to use state mgmt framework for that, but since there is not so much time...
+ * Would be better to use state mgmt framework for that, but since there is not so much time...
+ * Also, just selected user is stored. Potentially pagination object and user list could be managed.
  */
 @Injectable({
   providedIn: 'root'
