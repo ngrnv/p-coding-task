bb53a41b4624717c1f6044131b8a7e5a7be28d3b
Alexey Nagornov <al.nagornov@gmail.com>
2019-07-24 18:24:28 +0400

    add user card

--- src/modules/user/resolvers/user-resolver.ts
+++ src/modules/user/resolvers/user-resolver.ts
@@ -3,17 +3,18 @@ import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { ApiService } from '../../core/services';
 import { UsersStoreService } from '../../core/store/users-store.service';
 import { switchMap, take } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { User } from 'src/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserResolver implements Resolve<any> {
+export class UserResolver implements Resolve<User> {
 
   constructor(private apiService: ApiService, private userStore: UsersStoreService) {
   }
 
-  resolve(route: ActivatedRouteSnapshot): any {
+  resolve(route: ActivatedRouteSnapshot): Observable<User> {
     const userId: number = route.params.id;
     return this.userStore.selectedUser$.pipe(
       switchMap(user => {
--- src/modules/users-list/resolvers/users.resolver.ts
+++ src/modules/users-list/resolvers/users.resolver.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { ApiService } from '../../core/services';
+import { Observable } from 'rxjs';
+import { PaginatedApiResponse, User } from '../../../interfaces';
 
 @Injectable()
-export class UsersResolver implements Resolve<any> {
+export class UsersResolver implements Resolve<PaginatedApiResponse<User>> {
 
   constructor(private apiService: ApiService) {
   }
 
-  resolve(route: ActivatedRouteSnapshot): any {
+  resolve(route: ActivatedRouteSnapshot): Observable<PaginatedApiResponse<User>> {
     const page: number = route.queryParams['page'] || 1;
     return this.apiService.fetchUsers(page);
   }
